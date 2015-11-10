061b037dad08b6401d6c41cb8243d8b04811163c
Qingkang Zhang <qzhang72@illinois.edu>
2015-11-10 00:15:23 -0600

    me

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3')
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.path.join(BASE_DIR,'db.sqlite3')
+#     }
+# }
 
 
 # Internationalization
